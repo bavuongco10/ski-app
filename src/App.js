c332aa6902eda4ce4b28639e0ca7061f139228f4
Vuong Bui Ba Thanh <bavuongco10@gmail.com>
2018-03-10 17:10:15 +0700

    feat(SetUp): test lint-staged

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import logo from './resources/logo.svg';
 import './App.css';
 
@@ -11,7 +12,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to. lldsfsdfsdfj rjgjkghdkj djkfgdjk
+          To get started, edit <code>src/App.js</code> and save to. lldsfsdfsdfj
+          rjgjkghdkj djkfgdjk
         </p>
       </div>
     );
