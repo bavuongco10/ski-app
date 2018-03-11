08d2c2d361ccc32e1ca32d7b17c78d792b8ed532
Vuong Bui Ba Thanh <bavuongco10@gmail.com>
2018-03-11 17:58:31 +0700

    feat: home placeHolder

--- src/Containers/App/App.container.js
+++ src/Containers/App/App.container.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
+import Home from '../Home/Home.container';
 import HeaderReact from '../../Components/HeaderReact/HeaderReact.component';
 
 const App = () => (
   <div className="App">
     <HeaderReact />
+    <main>
+      <Route exact path="/" component={Home} />
+    </main>
   </div>
 );
 export default App;
